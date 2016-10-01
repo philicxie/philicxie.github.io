62cc90a266f9ea7c984fcf3fea0659272afffd52
Philic <xlw125414713@163.com>
2016-10-01 18:45:28 +0800

    daily update

--- js/index.js
+++ js/index.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
    console.log("hehe");
-    $.ajax({
-        url: "http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json&ip=123.123.123.123",
-        type: "GET",
-        dataType: "json",
-        success: function(json){
-            console.log(json);
+
+
+    var geolocation = new BMap.Geolocation();
+    geolocation.getCurrentPosition(function(r){
+        if(this.getStatus() == BMAP_STATUS_SUCCESS){
+            $.ajax({
+                url: "http://api.openweathermap.org/data/2.5/weather?appid=5471c7b82f8273ec8e7eb428e263e172&lat="+r.point.lat+"&lon="+r.point.lng,
+                type: "GET",
+                dataType: "json",
+                success: function(json){
+                    console.log("got location");
+                    console.log(json);
+                }
+            });
         }
-    });
+        else {
+            console.log("Error in getting geolocation.");
+        }
+    },{enableHighAccuracy: false})
 });

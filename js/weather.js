b85ecd492dec02e53fe1419e11bdbd14e14bf4c8
philicxie <philicxie@163.com>
2026-04-08 23:05:04 +0800

    delete unused

--- js/weather.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by philic on 2016/10/1.
- */
--- main.js
+++
@@ -1,5 +0,0 @@
-import __wbg_init from './js/mygame.js';
-import {initSync} from "./js/mygame.js";
-
-// initSync();
-__wbg_init().then(r => initSync());

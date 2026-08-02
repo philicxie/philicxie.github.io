80f6de6023e7d05221845f4789a038bb828a0c98
philicxie <philicxie@163.com>
2026-08-02 19:52:05 +0800

    blog init

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,26 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1785671344',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
